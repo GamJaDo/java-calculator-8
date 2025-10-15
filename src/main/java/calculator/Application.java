a8e6ac3ae4c348d238f2877cb4642d25377966a0
unknown <kim0615157@gmail.com>
2025-10-16 02:31:42 +0900

    feat:기능1 작성(문자열을 입력 받아 기본구분자를 분리하여 숫자의 합을 구함)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String expression = Console.readLine();
+
+        if (expression.isBlank()) {
+            System.out.println("0");
+        } else {
+            System.out.println("결과 : " + parseExpression(expression));
+        }
+    }
+
+    public static int parseExpression(String expression) {
+        int sum = 0;
+        String separator = "[,:]";
+
+        String[] tokens = expression.split(separator);
+
+        for (String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
+
+        return sum;
     }
 }
